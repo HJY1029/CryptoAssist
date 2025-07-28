3c39589a5da7b33a1f876957d6d2f78b023fe70d
HJY1029 <139771395+HJY1029@users.noreply.github.com>
2025-07-28 13:59:18 +0800

    Delete examples directory

--- examples/aes_openssl_example.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Cipher import AES
-from base64 import b64encode
-
-key = b'1234567890abcdef'
-data = b'hello world12345'
-cipher = AES.new(key, AES.MODE_ECB)
-encrypted = cipher.encrypt(data)
-print("密文:", b64encode(encrypted).decode())
